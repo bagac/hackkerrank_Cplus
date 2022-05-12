baa52a0ff845a74f4b6b930afec21e61f9aefd53
LGE\hung9.nguyen <hung9.nguyen@lge.com>
2022-05-12 17:36:18 +0700

    next

---
+++ 06. mang_2_chieu/Magic_square.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n = 3; 
+    int a[n][n];
+    for( int i = 0; i < n; i++){
+        for( int j = 0; j < n; j++){
+            cin >> a[i][j];
+        }
+    }
+    int sum_magic = 0;
+    for( int i = 0; i < n; i++) sum_magic += a[i][0];
+}
